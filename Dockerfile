0fbeea0335e5ce9018228a2c6d36459b1fe9b359
Utsav Kumar Jha <utsavkumarjha@Utsav-MacBook-Pro.local>
2019-09-09 16:24:07 +0200

    Added requirements file logic for notebook resolving

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM python:3.6
 
+RUN apt-get update
+
+RUN apt-get -y install nano
 RUN mkdir /code
 
 WORKDIR /code
 COPY . .
 
 RUN pip3 install papermill jupyterlab
-
 
 #CMD ["sleep", "50000"]
 ENTRYPOINT ["scripts/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,8 @@
 
 # bash script that runs papermill
 # the arguments will arrive from the github actions
+
+# these 2 exports are required to ensure python building- apprently there is a bug in ASCII encoding in python3 and this is the temporay fix
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 NOTEBOOK_PATHS="/code/notebooks/*"
@@ -15,6 +17,18 @@ do
     baseName=`basename ${notebook} .ipynb`
     paramFilename="/code/parameters/${baseName}"
 
+    # Install dependencies from requirements file
+    requirementsFileName="/code/requirements/${baseName}.txt"
+    echo "requirements file located at: ${requirementsFileName}"
+
+    if [[ -f "${requirementsFileName}" ]]; then
+        echo "Installing dependencies from: ${requirementsFileName}"
+        bash -c "pip3 install -r ${requirementsFileName}"
+    else
+        echo "No requirements file found for: ${notebook}"
+    fi
+
+
     # now we need the contents of the params file in -p format
     while read line;
     do
@@ -26,7 +40,6 @@ do
 
     echo "papermill ${notebook} ${outputFileName} ${papermill_argument}"
 
-    papermill "${notebook}" "${outputFileName} ${papermill_argument}"
+    bash -c "papermill ${notebook} ${outputFileName} ${papermill_argument}"
 done
-
-sleep infinity
+#sleep infinity
